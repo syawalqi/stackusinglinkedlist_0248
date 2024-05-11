c1aabf2571779f5c61791608df64a3a4884fe2d3
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-11 09:27:58 +0700

    membuat switch case 2

--- stackedlisnkedlist.cpp
+++ stackedlisnkedlist.cpp
@@ -72,3 +72,10 @@ int main() {
                 cin >> value;
                 stack.push(value); // Push the entered value onto the stack
                 break;
+                case 2:
+                if (!stack.isEmpty()) {
+                    stack.pop(); // Pop the top element from the stack
+                } else {
+                    cout << "Stack is empty. Cannot pop." << endl;
+                }
+                break;
