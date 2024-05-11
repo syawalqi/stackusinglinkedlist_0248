1bf8b9d6621dc8d4540055d663d5581f84f44144
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-11 09:28:13 +0700

    membuat case 3

--- stackedlisnkedlist.cpp
+++ stackedlisnkedlist.cpp
@@ -79,3 +79,10 @@ int main() {
                     cout << "Stack is empty. Cannot pop." << endl;
                 }
                 break;
+                 case 3:
+                if (!stack.isEmpty()) {
+                    stack.peek(); // Get the value of the top element
+                } else {
+                    cout << "Stack is empty. No top value." << endl;
+                }
+                break;
